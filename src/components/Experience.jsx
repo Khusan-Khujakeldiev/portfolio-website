8eee173bdbccdbaebb1887286142c7279d0866b4
Khusan-Khujakeldiev <Akatsuki-xus@mail.ru>
2025-03-20 00:53:12 +0200

    Add removed H1 in experince component

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -9,7 +9,9 @@ function Experience() {
         initial={{ opacity: 0, y: -100 }}
         transition={{ duration: 0.5 }}
         className=" my-20 text-center text-4xl"
-      ></motion.h1>
+      >
+        EXPERIENCE
+      </motion.h1>
       <div>
         {EXPERIENCES.map((experience, index) => (
           <div key={index} className=" mb-8 flex flex-wrap lg:justify-center">
